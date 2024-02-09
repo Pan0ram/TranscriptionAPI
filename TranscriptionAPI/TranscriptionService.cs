b14aa3be84d8c6c9ce4b6d1f0a88be124946ee34
Maik Stenzel <stenzel.maik@gmail.com>
2024-02-09 13:14:16 +0100

    Add TranscriptionService Class

---
+++ TranscriptionAPI/TranscriptionService.cs
@@ -0,0 +1,10 @@
+﻿namespace TranscriptionAPI
+{
+    public class TranscriptionService
+    {
+        public string TranscribeFromYoutube(string youtubeURL)
+        {
+            return "Meine Transkription. Blablabla test 123";
+        }
+    }
+}
