bae45a4c455e3e724cdf0c54045987407b819435
Maik Stenzel <stenzel.maik@gmail.com>
2024-02-12 10:53:42 +0100

    In Program.cs , builder hinzugefügt, Fehler Code CS1061,CS0118-- Debugging erklärt noch nicht was ich falsch mache

--- TranscriptionAPI/ApplicationDbContext.cs
+++ TranscriptionAPI/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationDbContext
 {
+    public static Microsoft.Extensions.DependencyInjection.IServiceCollection AddAuthorization(this Microsoft.Extensions.DependencyInjection.IServiceCollection services);
+
+    public static Microsoft.AspNetCore.Builder.IEndpointConventionBuilder MapIdentityApi<TUser>(this Microsoft.AspNetCore.Routing.IEndpointRouteBuilder endpoints) where TUser : class, new();
+
     public class ApplicationDbContext : IdentityDBContext<IndentiyUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
